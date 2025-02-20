8553c2ed61722c9c54d0937be995e3d24e6279df
Fatony Ahmad Fauzi <71713375+fatonyahmadfauzi@users.noreply.github.com>
2025-02-20 21:46:24 +0700

    menghapus fungsi deteksi fakeloop

--- public/js/main.js
+++ public/js/main.js
@@ -13,21 +13,13 @@ function sanitizeInput(input) {
             alert("Fitur ini dinonaktifkan!");
         }
     });
-
-    // Deteksi Developer Tools menggunakan Fake Loop
-    setInterval(function() {
-        if (window.outerWidth - window.innerWidth > 200 || window.outerHeight - window.innerHeight > 200) {
-            document.body.innerHTML = "<h1>Developer tools terdeteksi! Akses diblokir.</h1>";
-        }
-    }, 1000);
 })();
 
 // Menampilkan peringatan di Console Developer
 (function() {
     console.log("%c Berhenti!", "color: red; font-size: 50px; font-weight: bold;");
     console.log(
-        "%cIni adalah fitur browser yang ditujukan untuk developer. Jika seseorang meminta Anda untuk menyalin-menempel sesuatu di sini untuk mengaktifkan fitur Facebook atau 'meretas' akun seseorang, ini adalah penipuan dan akan memberikannya akses ke akun Anda.",
+        "%cIni adalah fitur browser yang ditujukan untuk developer. Jika seseorang meminta Anda untuk menyalin-menempel sesuatu di sini untuk mengaktifkan fitur Websites atau 'meretas' akun seseorang, ini adalah penipuan dan akan memberikannya akses ke akun Anda.",
         "color: white; background: black; font-size: 16px; padding: 10px;"
     );
-    console.log("Lihat https://www.facebook.com/selfxss untuk informasi selengkapnya.");
 })();
