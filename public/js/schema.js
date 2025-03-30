1e2d8b72392c753d4a713e23ba2f4c6e7d8af65d
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-03-30 07:17:27 +0700

    add schema

--- public/js/schema.js
+++ public/js/schema.js
@@ -4,6 +4,7 @@ const schemaData = [
         "@context": "https://schema.org",
         "@type": "Person",
         "name": "Fatony Ahmad Fauzi",
+        "alternateName": "fatonyahmadfauzi",
         "url": "https://fatonyahmadfauzi.me/",
         "sameAs": [
             "https://www.linkedin.com/in/fatonyahmadfauzi",
@@ -11,13 +12,40 @@ const schemaData = [
             "https://www.instagram.com/fatonyahmadfauzi",
             "https://www.tiktok.com/@fatonyahmadfauzi",
             "https://twitter.com/fatonyahmad89",
-            "https://facebook.com/fatonyahmadfauzi"
+            "https://facebook.com/fatonyahmadfauzi",
+            "https://www.youtube.com/@fatonyahmadfauzi",
+            "https://www.twitch.tv/fatonyahmadfauzi",
+            "https://www.threads.net/@fatonyahmadfauzi",
+            "https://shopee.co.id/fatonyahmad88",
+            "https://wa.me/6289638230725",
+            "https://t.me/fatonyahmadfauzi",
+            "https://www.scribd.com/document/Fatony-Ahmad-Fauzi",
+            "https://discord.com/users/554284939055726603",
+            "https://steamcommunity.com/id/fatonyahmadfauzi"
         ],
         "jobTitle": "Web Developer",
         "worksFor": {
             "@type": "Organization",
             "name": "Freelance"
-        }
+        },
+        "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Cikahuripan, Kabupaten Bogor",
+            "addressRegion": "Jawa Barat",
+            "postalCode": "16710",
+            "addressCountry": "ID"
+        },
+        "contactPoint": [
+            {
+                "@type": "ContactPoint",
+                "telephone": "+6289638230725",
+                "contactType": "Pribadi",
+                "contactOption": "WhatsApp",
+                "email": "fatonyahmadfauzi@gmail.com",
+                "areaServed": "Indonesia",
+                "availableLanguage": ["Bahasa Indonesia", "English"]
+            }
+        ]
     },
     {
         "@context": "https://schema.org",
