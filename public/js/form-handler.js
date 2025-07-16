3f0ccfae0695dc8d9a4cf523708f1a81a166ea4d
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-07-17 04:35:07 +0700

    update link email services

--- public/js/form-handler.js
+++ public/js/form-handler.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
             };
 
             try {
-                const response = await fetch("https://faa-form-backend-production.up.railway.app/handle-form.php", {
+                const response = await fetch("faa-form-backend.vercel.app/api/handle-form", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
